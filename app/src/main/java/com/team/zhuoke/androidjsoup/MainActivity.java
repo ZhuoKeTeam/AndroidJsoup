13763457a5b7076c5b2efbc8d20df5dc86c93035
Yunr <yun1185448859@qq.com>
2016-11-09 14:21:34 +0800

    add Yunr parse demo

--- app/src/main/java/com/team/zhuoke/androidjsoup/MainActivity.java
+++ app/src/main/java/com/team/zhuoke/androidjsoup/MainActivity.java
@@ -1,13 +1,35 @@
 package com.team.zhuoke.androidjsoup;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputLayout inputLayout;
+    private Button parse;
+    private TextView text;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            MyData data = (MyData) msg.obj;
+            text.setText(data.toString());
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +37,77 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initView();
+        parse.setOnClickListener(this);
+        //String url = "https://gupiao.caimao.com/weixin/note/reader/view/53103";
     }
 
+    private void initView() {
+        inputLayout = (TextInputLayout) findViewById(R.id.url_edit);
+        parse = (Button) findViewById(R.id.parse);
+        text = (TextView) findViewById(R.id.show_text);
+    }
+
+    @Override
+    public void onClick(View view) {
+        final String url = inputLayout.getEditText().getText().toString();
+        Log.e("---", url);
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                parse(url);
+            }
+        };
+
+        new Thread(runnable).start();
+    }
+
+    /**
+     * 解析数据
+     *
+     * @param url
+     */
+    private void parse(String url) {
+        Document html = null;
+        try {
+            html = Jsoup.parse(new URL(url), 10 * 1000);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Element body = html.body();
+        // NoteId
+        Element noteId = body.getElementById("doc_section");
+        String noteIdStr = noteId.attr("data-note-id");
+        // 预期收益
+        Element expected = body.getElementsByClass("note_detail").first()
+                .getElementsByClass("grid").get(1).getElementsByTag("p").first();
+        String exString = expected.text();
+        // 当前收益
+        Element current = body.getElementsByClass("note_detail").first()
+                .getElementsByClass("grid").last().getElementsByTag("p").first();
+        String currString = current.text();
+        // 止损
+        Element loss = body.getElementsByClass("note_info").first()
+                .getElementsByClass("group").last().getElementsByClass("content").first();
+        String lossString = loss.text();
+        // 价格
+        Element price = body.select("a.btn_red.full.fill.huge.m_t_10.no_radius").first();
+        String priceString = price.text();
+        MyData data = new MyData(noteIdStr, exString, currString, lossString, priceString);
+
+        Message msg = new Message();
+        msg.obj = data;
+        handler.sendMessage(msg);
+
+        //保存吧
+        saveData(data);
+    }
+
+    /**
+     * 保存数据
+     */
+    private void saveData(MyData data) {
+
+    }
 }
---
+++ app/src/main/java/com/team/zhuoke/androidjsoup/MyData.java
@@ -0,0 +1,70 @@
+package com.team.zhuoke.androidjsoup;
+
+/**
+ * Created by Yunr on 2016/11/9.
+ */
+
+public class MyData {
+
+    private String noteId;//NoteId
+    private String expected;//预期收益
+    private String current;//当前收益
+    private String loss;//止损
+    private String price;//价格
+
+    public MyData(String noteId, String expected, String current, String loss, String price) {
+        this.noteId = noteId;
+        this.expected = expected;
+        this.current = current;
+        this.loss = loss;
+        this.price = price;
+    }
+
+
+    public String getNoteId() {
+        return noteId;
+    }
+
+    public void setNoteId(String noteId) {
+        this.noteId = noteId;
+    }
+
+    public String getExpected() {
+        return expected;
+    }
+
+    public void setExpected(String expected) {
+        this.expected = expected;
+    }
+
+    public String getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(String current) {
+        this.current = current;
+    }
+
+    public String getLoss() {
+        return loss;
+    }
+
+    public void setLoss(String loss) {
+        this.loss = loss;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "NoteId:" + getNoteId() + "\n预期收益:" + getExpected() + "\n当前收益:"
+                + getCurrent() + "\n止损:" + getLoss() + "\n价格:" + getPrice();
+    }
+}
