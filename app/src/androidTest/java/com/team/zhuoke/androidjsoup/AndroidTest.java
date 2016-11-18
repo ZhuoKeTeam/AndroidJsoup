8f414f044ad0cc57cfac8dd6ca72b83ede6d6d3c
wangqing <wangqing@kaolafm.com>
2016-11-18 15:36:30 +0800

    1. 添加单元测试。

--- app/src/androidTest/java/com/team/zhuoke/androidjsoup/AndroidTest.java
+++ app/src/androidTest/java/com/team/zhuoke/androidjsoup/AndroidTest.java
@@ -4,6 +4,12 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+import com.zhy.http.okhttp.request.RequestCall;
+
+import org.junit.After;
+import org.junit.Before;
 import com.team.zhuoke.androidjsoup.db.DBUtils;
 import com.team.zhuoke.androidjsoup.db.interfaces.IBaseService;
 import com.team.zhuoke.androidjsoup.db.interfaces.IMyDataService;
@@ -13,6 +19,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import okhttp3.Call;
 
 import java.util.List;
 
@@ -29,6 +37,16 @@ public class AndroidTest {
 
     private static final String TAG = "AndroidTest";
 
+    @Before
+    public void init() {
+        Log.d(TAG, "init() called");
+    }
+
+    @After
+    public void end() {
+        Log.d(TAG, "end() called");
+    }
+
     private IMyDataService myDataService = null;
 
     private IBaseService baseService = null;
@@ -44,6 +62,16 @@ public class AndroidTest {
     }
 
     @Test
+    public void getContext() {
+        Log.i(TAG, "getContext: ");
+
+        final Context context = InstrumentationRegistry.getTargetContext();
+        new Handler(context.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                    Toast.makeText(context, "显示时间很短： hello", Toast.LENGTH_LONG).show();
+            }
+        });
     public void testInsert(){
         clearTable();
         MyData generateData = generateOneData();
@@ -118,6 +146,28 @@ public class AndroidTest {
         testMyData.setEndTime("2016-11-18 15:08");
         testMyData.setPageAddress("http://www.baidu.com");
         return testMyData;
+    }
+
+    @Test
+    public void testNetWork() {
+        Log.d(TAG, "testNetWork() called");
+        String url = "http://www.baidu.com";
+        RequestCall requestCall = OkHttpUtils.get().url(url).build();
+
+        Log.i(TAG, "testNetWork: " + url);
+
+        requestCall.execute(new StringCallback() {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                Log.d(TAG, "onError() called with: call = [" + call + "], e = [" + e + "], id = [" + id + "]");
+            }
+
+            @Override
+            public void onResponse(String response, int id) {
+                Log.d(TAG, "onResponse() called with: response = [" + response + "], id = [" + id + "]");
+            }
+        });
+        Log.i(TAG, "testNetWork: 完成" );
     }
 
 }
--- app/src/main/java/com/team/zhuoke/androidjsoup/IApplication.java
+++ app/src/main/java/com/team/zhuoke/androidjsoup/IApplication.java
@@ -8,6 +8,11 @@ import com.facebook.stetho.Stetho;
 import com.facebook.stetho.dumpapp.DumperPlugin;
 import com.facebook.stetho.dumpapp.plugins.HprofDumperPlugin;
 import com.team.zhuoke.androidjsoup.db.DBUtils;
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by ztw on 2016/11/9.
@@ -40,5 +45,15 @@ public class IApplication extends Application{
                 .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(mContext))
                 .build());
         Stetho.initializeWithDefaults(mContext);
+
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
     }
 }
